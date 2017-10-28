782925b7b113af7beeaa9b0572ea9a85b6ef7d75
venky <vgopal3@jhu.edu>
2017-10-28 17:43:56 -0400

    Checking in primality test

--- rsa-keygen.go
+++ rsa-keygen.go
@@ -2,17 +2,23 @@ package main
 
 import (
   "fmt"
-  "math/rand"
+  //"math/rand"
   "strconv"
 )
 
 func main() {
 
-  randomNumber := rand.Int31n(65536)
+  //randomNumber := rand.Int31n(65536)
+  randomNumber := 281
   fmt.Println("Random number is ", randomNumber)
 
   exponentiatedValue := squareAndMultiple(7,4,13)
   fmt.Println(exponentiatedValue)
+
+  // Check for a prime number
+  // I'm hardcoding the value of K in primality test to 5
+  accuracyFactor := 5;
+  isaPrimeNumber(randomNumber,accuracyFactor)
 
 }
 
@@ -24,7 +30,6 @@ func squareAndMultiple(a int, b int, c int) (int) {
 
   initialValue := a % c
   result := initialValue
-
   // Using the square and multipy algorithm to perform modular exponentation
   for i := 1; i < binExpLength; i++ {
 
@@ -36,5 +41,38 @@ func squareAndMultiple(a int, b int, c int) (int) {
     }
   }
   return result
+
+}
+
+func isaPrimeNumber(number int, accuracyFactor int) {
+
+  // First finding the value of r, d as per equation ;
+  // d * 2pow(r) = n - 1
+
+  varNumber := float64(number - 1)
+  r := 2
+  // exponentitalR is 2powr(r)
+  exponentitalR := float64(2)
+
+  for true {
+    x := varNumber/exponentitalR
+    fmt.Println(x)
+    if (x == float64(int64(x))) {
+    // Fixing value 10000000000 for calculation purpose
+    // To resue the squareAndMultiple algorithm but not affect the modulo part
+      r = r + 1
+      exponentitalR = float64(squareAndMultiple(2,r, 10000000000))
+      fmt.Println("exponentitalR is ", exponentitalR, " and R is ", r)
+      } else {
+        break
+      }
+
+    }
+
+  r = r - 1
+  exponentitalR = float64(squareAndMultiple(2,r, 10000000000))
+  d := (varNumber/exponentitalR)
+
+
 
 }
